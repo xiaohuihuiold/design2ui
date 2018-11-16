278d24ba78b82463416cdaaf55f010587072ee89
xiaohuihuiold <xiaohuihuiold@gmail.com>
2018-11-16 19:26:08 +0800

    更新README.md

--- test/design2ui_test.dart
+++ test/design2ui_test.dart
@@ -1,13 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:design2ui/design2ui.dart';
 
 void main() {
-  test('adds one to input values', () {
-    pr(b: 5);
-  });
-}
 
-void pr({int a,int b}){
-  print(a);
 }
